841ab14f5c97da5ac82a7788e7127a2c3059f823
vladimirtischler <tischler.vladimir@gmail.com>
2019-10-20 17:54:14 +0200

    open-lab-02.5

---
+++ Open-lab-02.5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static bool Reverse(int a)
+
+        {
+            if (a % 5 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return !true;
+            }
+        }
+        static void Main(string[] args)
+        {
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Reverse(b));
+
+        }
+    }
+}
