13ddd33623aecc3b2f3367ea9e4dc1475ca22946
vladimirtischler <56106321+vladimirtischler@users.noreply.github.com>
2019-10-20 19:05:31 +0200

    open-lab-02.5

--- Open-lab-02.5/Program.cs
+++ Open-lab-02.5/Program.cs
@@ -1,19 +1,19 @@
-﻿using System;
+using System;
 
 namespace ConsoleApp1
 {
     class Program
     {
-        public static bool Reverse(int a)
+        public static bool DivisibleByFive(int a)
 
         {
-           return (a % 5 == 0);
-            
+            return (a % 5 == 0);
+
         }
         static void Main(string[] args)
         {
             int b = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(Reverse(b));
+            Console.WriteLine(DivisibleByFive(b));
 
         }
     }
